97b73d957c4d3ed7bc115a6297c09412d0c80c14
Faby12345 <108344307+Faby12345@users.noreply.github.com>
2023-06-08 23:01:55 +0300

    Add files via upload

---
+++ CurrencysDB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using SQLite;
+namespace mainApp
+{
+    class CurrencysDB
+    {
+        public CurrencysDB() { }
+        public string Name { get; set; }
+        public double Fixing { get; set; }
+        public double SellPrice { get; set; }
+        public double BuyPrice { get; set; }
+        public string Symbol { get; set; }
+        public double SellCom { get; set; }
+        public double BuyCom { get; set; }
+        
+
+   
+    }
+}
